b08d073bfdcf12f6857bb21a5aab09fc39366290
ntirabassi <89865902+ntirabassi@users.noreply.github.com>
2022-02-28 21:08:38 -0500

    Add files via upload

---
+++ HuttHikeProgram.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x;
+
+x = 0;
+
+for (x = 0; x <= 100; x++) 
+{
+    if (x % 3 == 0)
+    {
+            Console.WriteLine("Hutt"); 
+    }
+    else if (x % 7 == 0)
+    {
+        Console.WriteLine("Hike");
+    }
+    else
+    { 
+        Console.WriteLine(x);
+    }
+
+    
+}
+
+Console.WriteLine("End.");
+
+Console.ReadKey();
