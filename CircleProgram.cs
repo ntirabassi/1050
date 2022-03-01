efa2b984e30aa0333fc115e87bf41ff7f869c5f8
ntirabassi <89865902+ntirabassi@users.noreply.github.com>
2022-02-28 21:06:39 -0500

    Add files via upload

---
+++ CircleProgram.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+double x;
+double c;
+
+Console.Write("Type the radius of a circle and press enter: ");
+x = Convert.ToDouble(Console.ReadLine());
+
+c = x * x * 3.14;
+
+Console.Write("The area is ");
+Console.Write(c);
+Console.WriteLine(".");
+
+c = 2 * 3.14 * x;
+
+Console.Write("The circumference is ");
+Console.Write(c);
+Console.WriteLine(".");
+
+Console.ReadKey(); 
+
+
