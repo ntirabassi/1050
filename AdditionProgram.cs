f56987b3adc94069644e9d6e268f45c7c295959a
ntirabassi <89865902+ntirabassi@users.noreply.github.com>
2022-02-28 21:06:05 -0500

    Add files via upload

---
+++ AdditionProgram.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+double x;
+double y;
+double z;
+
+
+Console.Write("Type in a number from 1 to 10: ");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Type in a number from 10 to 100: ");
+y = Convert.ToDouble(Console.ReadLine());
+
+z = x + y;
+
+Console.Write("The sum of these numbers is ");
+Console.Write(z);
+
+Console.ReadKey();
+
+
