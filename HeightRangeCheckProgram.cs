8bb99ee9a081a487727985b1c64fafe4b0ae345b
ntirabassi <89865902+ntirabassi@users.noreply.github.com>
2022-02-28 21:07:15 -0500

    Add files via upload

---
+++ HeightRangeCheckProgram.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double height;
+
+Console.WriteLine("How tall are you in inches?");
+Console.Write("Type a number and press enter: ");
+height = Convert.ToDouble(Console.ReadLine());
+
+if (height < 60 || height > 75)
+{
+   Console.WriteLine("You do not have an average height!");
+}
+else
+{
+    Console.WriteLine("You have a normal height.");
+}
+
+Console.ReadKey();  
